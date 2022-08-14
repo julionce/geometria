473edffe9608ab027f23f98bfe3282ea20d28707
julionce <julianbortega@gmail.com>
2022-08-14 18:58:02 +0200

    Add Debug and Display bounds to rhino::Deserialize::Error type

--- src/serializer/rhino/deserialize.rs
+++ src/serializer/rhino/deserialize.rs
@@ -1,10 +1,13 @@
+use std::fmt::Debug;
+use std::fmt::Display;
+
 use super::deserializer::Deserializer;
 
 pub trait Deserialize
 where
     Self: Sized,
 {
-    type Error;
+    type Error: Debug + Display;
 
     fn deserialize<D>(deserializer: &mut D) -> Result<Self, Self::Error>
     where
