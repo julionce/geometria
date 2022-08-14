f2b294911d9bdd34ebda2ce9d1d88c952023d57e
julionce <julianbortega@gmail.com>
2022-08-14 19:00:12 +0200

    Create rhino::header::HeaderError type and add some deserialization test for rhino::header::Header type

--- src/serializer/rhino/header.rs
+++ src/serializer/rhino/header.rs
@@ -1,12 +1,29 @@
+use std::fmt::Display;
+
 use super::deserialize::Deserialize;
 use super::deserializer::Deserializer;
 
 pub struct Header;
 
+#[derive(Debug, PartialEq)]
+pub enum HeaderError {
+    InvalidHeader,
+    IoError(std::io::ErrorKind),
+}
+
+impl Display for HeaderError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::InvalidHeader => write!(f, "invalid header"),
+            Self::IoError(e) => write!(f, "{} deserializing the header", e),
+        }
+    }
+}
+
 const FILE_BEGIN: &[u8] = "3D Geometry File Format ".as_bytes();
 
 impl Deserialize for Header {
-    type Error = String;
+    type Error = HeaderError;
 
     fn deserialize<D>(deserializer: &mut D) -> Result<Self, Self::Error>
     where
@@ -16,9 +33,62 @@ impl Deserialize for Header {
         match deserializer.read_exact(&mut buffer) {
             Ok(()) => match FILE_BEGIN == buffer {
                 true => Ok(Header {}),
-                false => Err("3dm file error: invalid file begin".to_string()),
+                false => Err(HeaderError::InvalidHeader),
             },
-            Err(e) => Err(format!("{}", e)),
+            Err(e) => Err(HeaderError::IoError(e.kind())),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io::Cursor;
+
+    use crate::serializer::rhino::{chunk, reader::Reader, version::Version};
+
+    use super::*;
+
+    #[test]
+    fn deserialize_ok() {
+        let data = "3D Geometry File Format ".as_bytes();
+        let mut deserializer = Reader {
+            stream: &mut Cursor::new(data),
+            version: Version::V1,
+            chunk_begin: chunk::Begin::default(),
+        };
+
+        assert!(Header::deserialize(&mut deserializer).is_ok());
+    }
+
+    #[test]
+    fn deserialize_invalid_header() {
+        let data = "4D Geometry File Format ".as_bytes();
+
+        let mut deserializer = Reader {
+            stream: &mut Cursor::new(data),
+            version: Version::V1,
+            chunk_begin: chunk::Begin::default(),
+        };
+
+        assert_eq!(
+            Header::deserialize(&mut deserializer).err(),
+            Some(HeaderError::InvalidHeader)
+        );
+    }
+
+    #[test]
+    fn deserialize_io_error() {
+        let data = "3D Geometry File Format".as_bytes();
+
+        let mut deserializer = Reader {
+            stream: &mut Cursor::new(data),
+            version: Version::V1,
+            chunk_begin: chunk::Begin::default(),
+        };
+
+        assert_eq!(
+            Header::deserialize(&mut deserializer).err(),
+            Some(HeaderError::IoError(std::io::ErrorKind::UnexpectedEof))
+        );
     }
 }
