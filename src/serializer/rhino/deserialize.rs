c67d912b187186e49fdeb1aa69231e1e3b370849
julionce <julianbortega@gmail.com>
2022-08-15 15:15:00 +0200

    Implement rhino::deserialize::impl_deserialize_num macro

--- src/serializer/rhino/deserialize.rs
+++ src/serializer/rhino/deserialize.rs
@@ -1,5 +1,6 @@
 use std::fmt::Debug;
 use std::fmt::Display;
+use std::mem;
 
 use super::deserializer::Deserializer;
 
@@ -13,3 +14,40 @@ where
     where
         D: Deserializer;
 }
+
+macro_rules! impl_deserialize_num {
+    ($sty:ty) => {
+        impl Deserialize for $sty {
+            type Error = String;
+
+            fn deserialize<D>(deserializer: &mut D) -> Result<Self, Self::Error>
+            where
+                D: Deserializer,
+            {
+                let mut bytes = [0; mem::size_of::<Self>()];
+                match deserializer.read_exact(&mut bytes) {
+                    Ok(()) => Ok(Self::from_le_bytes(bytes)),
+                    Err(e) => Err(format!("{}", e)),
+                }
+            }
+        }
+    };
+}
+
+impl_deserialize_num! {u8}
+impl_deserialize_num! {u16}
+impl_deserialize_num! {u32}
+impl_deserialize_num! {u64}
+impl_deserialize_num! {u128}
+
+impl_deserialize_num! {i8}
+impl_deserialize_num! {i16}
+impl_deserialize_num! {i32}
+impl_deserialize_num! {i64}
+impl_deserialize_num! {i128}
+
+impl_deserialize_num! {usize}
+impl_deserialize_num! {isize}
+
+impl_deserialize_num! {f32}
+impl_deserialize_num! {f64}
