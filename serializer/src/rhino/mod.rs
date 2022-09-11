62f8638075de911a5a2ad4cd4179218ff087bd93
julionce <julianbortega@gmail.com>
2022-09-11 20:59:26 +0200

    Move rhino::Properties type to rhino/properties.rs file.

--- serializer/src/rhino/mod.rs
+++ serializer/src/rhino/mod.rs
@@ -1,4 +1,5 @@
 pub mod application;
+mod bool;
 pub mod chunk;
 mod comment;
 mod date;
@@ -7,6 +8,7 @@ mod deserializer;
 mod header;
 pub mod notes;
 mod on_version;
+mod properties;
 mod reader;
 pub mod revision_history;
 mod start_section;
@@ -15,103 +17,12 @@ mod time;
 mod typecode;
 mod version;
 
-use chunk::Chunk;
-use deserialize::Deserialize;
-use deserializer::Deserializer;
-use on_version::Version as OnVersion;
-use version::Version;
-
-use std::io::{Seek, SeekFrom};
-
-use self::{
-    application::Application, notes::Notes, revision_history::RevisionHistory,
-    string::WStringWithLength,
-};
-
-#[derive(Default)]
-struct Properties {
-    filename: String,
-    version: OnVersion,
-    revision_history: RevisionHistory,
-    notes: Notes,
-    application: Application,
-}
-
-impl<D> Deserialize<'_, D> for Properties
-where
-    D: Deserializer,
-{
-    type Error = String;
-
-    fn deserialize(deserializer: &mut D) -> Result<Self, Self::Error> {
-        let mut properties = Properties::default();
-        if Version::V1 == deserializer.version() {
-            deserializer.seek(SeekFrom::Start(32u64)).unwrap();
-            loop {
-                let mut chunk = Chunk::deserialize(deserializer)?;
-                match chunk.chunk_begin().typecode {
-                    typecode::COMMENTBLOCK => {
-                        // TODO: process _comment
-                        let _comment = String::deserialize(&mut chunk)?;
-                    }
-                    typecode::SUMMARY => {
-                        properties.revision_history = RevisionHistory::deserialize(&mut chunk)?;
-                    }
-                    typecode::NOTES => {
-                        properties.notes = Notes::deserialize(&mut chunk)?;
-                    }
-                    typecode::BITMAPPREVIEW | typecode::CURRENTLAYER | typecode::LAYER => {
-                        // TODO
-                    }
-                    _ => {
-                        break;
-                    }
-                }
-                chunk.seek(SeekFrom::End(1)).unwrap();
-            }
-        } else {
-            let mut properties_chunk = Chunk::deserialize(deserializer)?;
-            if typecode::PROPERTIES_TABLE == properties_chunk.chunk_begin().typecode {
-                loop {
-                    let mut chunk = Chunk::deserialize(&mut properties_chunk)?;
-                    match chunk.chunk_begin().typecode {
-                        typecode::PROPERTIES_OPENNURBS_VERSION => {
-                            properties.version = OnVersion::deserialize(&mut chunk)?;
-                        }
-                        typecode::PROPERTIES_AS_FILE_NAME => {
-                            properties.filename =
-                                WStringWithLength::deserialize(&mut chunk)?.into();
-                        }
-                        typecode::PROPERTIES_REVISIONHISTORY => {
-                            properties.revision_history = RevisionHistory::deserialize(&mut chunk)?;
-                        }
-                        typecode::PROPERTIES_NOTES => {
-                            properties.notes = Notes::deserialize(&mut chunk)?;
-                        }
-                        typecode::PROPERTIES_APPLICATION => {
-                            properties.application = Application::deserialize(&mut chunk)?;
-                        }
-                        typecode::PROPERTIES_PREVIEWIMAGE
-                        | typecode::PROPERTIES_COMPRESSED_PREVIEWIMAGE => {
-                            // TODO
-                        }
-                        _ => {
-                            break;
-                        }
-                    }
-                    chunk.seek(SeekFrom::End(1)).unwrap();
-                }
-            }
-            properties_chunk.seek(SeekFrom::End(1)).unwrap();
-        }
-        Ok(properties)
-    }
-}
-
 #[cfg(test)]
 mod tests {
-    use super::{comment::Comment, start_section::StartSection, *};
-    use header::Header;
+    use super::{
+        comment::Comment, deserialize::Deserialize, deserializer::Deserializer, header::Header,
+        properties::Properties, start_section::StartSection, version::Version, *,
+    };
     use reader::Reader;
     use std::fs::File;
 
---
+++ serializer/src/rhino/properties.rs
@@ -0,0 +1,64 @@
+use geometria_derive::Deserialize;
+use std::io::{Seek, SeekFrom};
+
+use super::{
+    application::Application, chunk::Chunk, deserialize::Deserialize, deserializer::Deserializer,
+    notes::Notes, on_version::Version as OnVersion, revision_history::RevisionHistory,
+    string::WStringWithLength, typecode, version::Version,
+};
+
+#[derive(Default, Deserialize)]
+#[table]
+pub struct PropertiesV1 {
+    #[table_field(COMMENTBLOCK)]
+    comment: String,
+    #[table_field(SUMMARY)]
+    revision_history: RevisionHistory,
+    #[table_field(NOTES)]
+    notes: Notes,
+}
+
+#[derive(Default, Deserialize)]
+#[table(PROPERTIES_TABLE)]
+pub struct PropertiesV2 {
+    #[table_field(PROPERTIES_AS_FILE_NAME)]
+    #[underlying_type(WStringWithLength)]
+    filename: String,
+    #[table_field(PROPERTIES_OPENNURBS_VERSION)]
+    version: OnVersion,
+    #[table_field(PROPERTIES_REVISIONHISTORY)]
+    revision_history: RevisionHistory,
+    #[table_field(PROPERTIES_NOTES)]
+    notes: Notes,
+    #[table_field(PROPERTIES_APPLICATION)]
+    application: Application,
+}
+
+pub enum Properties {
+    V1(PropertiesV1),
+    V2(PropertiesV2),
+}
+
+impl Default for Properties {
+    fn default() -> Self {
+        Self::V1(PropertiesV1::default())
+    }
+}
+
+impl<D> Deserialize<'_, D> for Properties
+where
+    D: Deserializer,
+{
+    type Error = String;
+
+    fn deserialize(deserializer: &mut D) -> Result<Self, Self::Error> {
+        let properties: Properties;
+        if Version::V1 == deserializer.version() {
+            deserializer.seek(SeekFrom::Start(32u64)).unwrap();
+            properties = Properties::V1(PropertiesV1::deserialize(deserializer)?);
+        } else {
+            properties = Properties::V2(PropertiesV2::deserialize(deserializer)?);
+        }
+        Ok(properties)
+    }
+}
